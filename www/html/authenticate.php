af8c86e4e8c216f79c3eeacb491a3e2aae2054ae
Nick Carroll <ncarroll@nd.edu>
2016-01-23 15:44:47 -0500

    added script authenticate

---
+++ www/html/authenticate.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "";
+$password = "";
+$dbname = "test";
+$username = "nick";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "SELECT * FROM loginTest WHERE name LIKE $username";
+$result = $conn->QUERY($sql);
+
+if ($result->num_rows > 0) {
+
+// out out data of each row
+
+while($row  =  $result->fetch_assoc()) {
+
+	echo "name : " . $row["name"];
+}
+} else {
+	echo "0 results";
+}
+
+$conn->close();
+?>
